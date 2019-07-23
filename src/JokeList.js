e0c323d501f46f41b9d4eb4a14258047c630a5c4
LeeTrent <lee.trent.1@gmail.com>
2019-07-23 09:22:04 -0500

    Styles 'Fetch Jokes' button in JokeList.js

--- src/JokeList.js
+++ src/JokeList.js
@@ -92,7 +92,7 @@ export default class JokeList extends Component {
                 <div className="JokeList-sidebar">
                     <h1 className="JokeList-title"><span>Dad</span> Jokes</h1>
                     <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' alt="Smiley Face" />
-                    <button className="JokeList-getmore" onClick={this.handleClick}>New Jokes</button>
+                    <button className="JokeList-getmore" onClick={this.handleClick}>Fetch Jokes</button>
                 </div> 
                 <div className="JokeList-jokes">
                     { sortedJokes.map(j => (
